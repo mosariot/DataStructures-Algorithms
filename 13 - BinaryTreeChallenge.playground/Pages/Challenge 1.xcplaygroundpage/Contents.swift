8b37ee59db5fa947884ba81869c6006a9f004d1f
mosariot <mosariot@gmail.com>
2022-01-02 14:45:53 +0300

    Chapter 13 completed

--- 13 - BinaryTreeChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
+++ 13 - BinaryTreeChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
@@ -11,26 +11,29 @@
  both the root and the furthest leaf.
  */
 var tree: BinaryNode<Int> = {
-  let zero = BinaryNode(value: 0)
-  let one = BinaryNode(value: 1)
-  let five = BinaryNode(value: 5)
-  let seven = BinaryNode(value: 7)
-  let eight = BinaryNode(value: 8)
-  let nine = BinaryNode(value: 9)
-  
-  seven.leftChild = one
-  one.leftChild = zero
-  one.rightChild = five
-  seven.rightChild = nine
-  nine.leftChild = eight
-  
-  return seven
+    let zero = BinaryNode(value: 0)
+    let one = BinaryNode(value: 1)
+    let five = BinaryNode(value: 5)
+    let seven = BinaryNode(value: 7)
+    let eight = BinaryNode(value: 8)
+    let nine = BinaryNode(value: 9)
+    
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+    
+    return seven
 }()
 
 print(tree)
 
-// Your code here
+func height<T>(of node: BinaryNode<T>?) -> Int {
+    guard node != nil else { return -1 }
+    return 1 + max(height(of: tree.leftChild), height(of: tree.rightChild))
+}
 
-// height(of: tree)
+height(of: tree)
 
 //: [Next Challenge](@next)
--- 13 - BinaryTreeChallenge.playground/Pages/Challenge 2.xcplaygroundpage/Contents.swift
+++ 13 - BinaryTreeChallenge.playground/Pages/Challenge 2.xcplaygroundpage/Contents.swift
@@ -24,30 +24,62 @@
  */
 
 var tree: BinaryNode<Int> = {
-  
-  let root = BinaryNode(value: 15)
-  let ten = BinaryNode(value: 10)
-  let five = BinaryNode(value: 5)
-  let twelve = BinaryNode(value: 12)
-  let twentyFive = BinaryNode(value: 25)
-  let seventeen = BinaryNode(value: 17)
-  
-  root.leftChild = ten
-  root.rightChild = twentyFive
-  ten.leftChild = five
-  ten.rightChild = twelve
-  twentyFive.leftChild = seventeen
-  
-  return root
+    
+    let root = BinaryNode(value: 15)
+    let ten = BinaryNode(value: 10)
+    let five = BinaryNode(value: 5)
+    let twelve = BinaryNode(value: 12)
+    let twentyFive = BinaryNode(value: 25)
+    let seventeen = BinaryNode(value: 17)
+    
+    root.leftChild = ten
+    root.rightChild = twentyFive
+    ten.leftChild = five
+    ten.rightChild = twelve
+    twentyFive.leftChild = seventeen
+    
+    return root
 }()
 
 print(tree)
 
-// Your code here
+extension BinaryNode {
+    
+    public func traversePreOrder(visit: (Element?) -> Void) {
+        visit(value)
+        if let leftChild = leftChild {
+            leftChild.traversePreOrder(visit: visit)
+        } else {
+            visit(nil)
+        }
+        if let rightChild = rightChild {
+            rightChild.traversePreOrder(visit: visit)
+        } else {
+            visit(nil)
+        }
+    }
+}
 
-/*
-let array = serialize(tree)
+func serialize<T>(_ node: BinaryNode<T>) -> [T?] {
+    var array: [T?] = []
+    node.traversePreOrder { array.append($0) }
+    return array
+}
+
+func deserialize<T>(_ array: inout [T?]) -> BinaryNode<T>? {
+    guard !array.isEmpty, let value = array.removeLast() else { return nil }
+    let node = BinaryNode(value: value)
+    node.leftChild = deserialize(&array)
+    node.rightChild = deserialize(&array)
+    return node
+}
+
+func deserialize<T>(_ array: [T?]) -> BinaryNode<T>? {
+    var reversed = Array(array.reversed())
+    return deserialize(&reversed)
+}
+
+var array = serialize(tree)
 print(array)
 let node = deserialize(array)
 print(node!)
-*/
