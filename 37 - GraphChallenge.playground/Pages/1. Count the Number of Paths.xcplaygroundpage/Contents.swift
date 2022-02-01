0f64de82ef1425c1f8a8fa0e673a4e3ab0427668
mosariot <mosariot@gmail.com>
2022-02-01 14:36:08 +0300

    Chapter 37 completed

--- 37 - GraphChallenge.playground/Pages/1. Count the Number of Paths.xcplaygroundpage/Contents.swift
+++ 37 - GraphChallenge.playground/Pages/1. Count the Number of Paths.xcplaygroundpage/Contents.swift
@@ -8,9 +8,31 @@
 
 extension Graph where Element: Hashable {
     
-    public func numberOfPaths(from source: Vertex<Element>, to destination: Vertex<Element>) -> Int {
-        // Implement Solution Here.
-        return 0
+    public func numberOfPaths(from source: Vertex<Element>,
+                              to destination: Vertex<Element>) -> Int {
+        
+        var numberOfPaths = 0
+        var visited: Set<Vertex<Element>> = []
+        paths(from: source, to: destination, visited: &visited, pathCount: &numberOfPaths)
+        return numberOfPaths
+    }
+    
+    private func paths(from source: Vertex<Element>,
+                       to destination: Vertex<Element>,
+                       visited: inout Set<Vertex<Element>>,
+                       pathCount: inout Int) {
+        visited.insert(source)
+        if source == destination {
+            pathCount += 1
+        } else {
+            let neighbors = edges(from: source)
+            for edge in neighbors {
+                if !visited.contains(edge.destination) {
+                    paths(from: edge.destination, to: destination, visited: &visited, pathCount: &pathCount)
+                }
+            }
+        }
+        visited.remove(source)
     }
 }
 
